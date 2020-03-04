7409a435c387c6c3d8b59273158738d1e3667bf2
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-03-04 19:14:53 +0900

    Update 2_highway.R

--- 데이터분석입문/PART6/rsciprt/2_highway.R
+++ 데이터분석입문/PART6/rsciprt/2_highway.R
@@ -21,10 +21,6 @@
   highway = read.csv('data/highway_1812.csv', fileEncoding='UTF-8')
   highway  
   
-  head(highway)
-  tail(highway)
-  names(highway)
-  
   # 집계일자를 날짜 형식으로 변환
   highway$집계일자 = strptime(highway$집계일자, format='%Y%m%d')
 
@@ -32,18 +28,20 @@
   # 요일 변수 추가하기 
   highway$요일 = weekdays(highway$집계일자)
   
-  
-
 #### 여기서부터 실습 입니다 ####
     
   
 ##1 변수이름, 관측치 샘플 확인하기
-  
-  
+  names(highway)
+  highway[1, ]
+  head(highway)
+  tail(highway)
   
 ##2 subset( )으로 토요일 데이터만 모아서 
 ##  highway_sat 로 저장하기
-  
+  highway$요일 == '토요일'
+  subset(highway, 요일=='토요일')
+  highway_sat = subset(highway, 요일=='토요일')
   
   
 ##3 영업소명 기준 '통영' 혹은 '북통영' 이면서
@@ -58,17 +56,17 @@
   ## (예제) &를 활용한 조건 결합
   (1:5 >= 3) & (1:5 <= 3)
   
-
+  ty_weekend = subset(highway, (영업소명 %in% c('통영', '북통영')) & (요일 %in% c('토요일', '일요일')) )
   
 ##4 aggregate( )로 요일별 통행량 합계 계산하기
   
-  
+  aggregate(통행량~요일,data=highway, sum)
   
   
   
 ##5 3에서 만든 ty_weekend로 집계시간별 통행량 합계를 계산하기
   
-  
+  agg = aggregate(통행량~집계시간,data=ty_weekend, sum)
   
   
 # End of script
