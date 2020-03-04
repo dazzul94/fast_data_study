9722fca3ebfa14bb8f5117ca4113547177ed9565
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-03-04 20:13:41 +0900

    심심해서 해본 경쟁률대비 커트라인예측

---
+++ 데이터분석입문/PART6/rsciprt/4_경쟁률대비커트라인.R
@@ -0,0 +1,22 @@
+library(openxlsx)
+
+compet = read.xlsx('data/역대경쟁률.xlsx')
+head(compet)
+
+# hist(compet$커트라인)
+# boxplot(compet$커트라인)
+
+plot(compet$경쟁률, compet$커트라인점수, pch=16, col='#3377BB77')
+abline(v=mean(compet$경쟁률), lty=2)
+abline(h=mean(compet$커트라인점수),lty=2)
+
+# 경쟁률이 낮을수록 커트라인은 낮고 
+# 경쟁률이 높을수록 커트라인이 높다.
+
+lm_compet = lm(커트라인점수 ~ 경쟁률, data=compet)
+summary(lm_compet)
+
+# Adjusted R-squared:  0.7098 
+
+# 2020 예측 커트라인은 
+# 0.37578 * 18.7 + 334.17416 = 341.201246
