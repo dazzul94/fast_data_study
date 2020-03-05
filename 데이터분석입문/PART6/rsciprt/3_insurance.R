6dacb99fd43dbf5b84008e255b55f236822b28de
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-03-05 19:02:20 +0900

    20200305

--- 데이터분석입문/PART6/rsciprt/3_insurance.R
+++ 데이터분석입문/PART6/rsciprt/3_insurance.R
@@ -24,37 +24,46 @@
   # 다운로드한 파일을 강의자료의 data 폴더에 넣기
 
 
-
 #### 여기서부터 실습 입니다 ####
  
 
 ##1 데이터 불러와서 insurance로 저장하기
 
-
+insurance = read.csv('data/insurance.csv');
 
 
 ##2 summary( )함수로 데이터 요약하고
 ##  수치형 변수는 히스토그램, 범주형 변수는 표 만들기
 
+summary(insurance)
 
+hist(insurance$age)
 
+table(insurance$region)
 
 ##3 bmi와 charges의 산점도 그리고 상관계수 계산하기
 
+plot(insurance$bmi, insurance$charges, pch=16)
 
-
+cor(insurance$bmi, insurance$charges) # 상관계수
 
 ##4 region별 charges의 상자그림 그리고 그룹별 평균 계산하기
 
-
+boxplot(charges ~ region, data=insurance)
+aggregate(charges ~ region, data=insurance, mean)
 
 
 ##5 lm( )과 rpart( )를 활용하여 
 ##  관심변수 charges를 나머지 변수로 설명하는 모형 적합하기 
 
+lm_ins = lm(charges ~ ., data=insurance)
 
+summary(lm_ins)
 
+library(rpart)
+tree_ins = rpart(charges ~., data=insurance)
 
-
+library(rpart.plot)
+rpart.plot(tree_ins)
 
 # End of script
